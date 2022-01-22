0e14ce626b40c2b926ff4f7861ebee729d537856
Zachary Luety <zrluety216@gmail.com>
2022-01-22 15:46:32 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # SwiftFinancial
 
-A description of this package.
+A collection of elementary financial functions inspired by the [numpy-financial](https://numpy.org/numpy-financial/latest/) library.
+
+## Currently the following fincials functions are supported
+
+|ipmt|Compute the interest portion of a payment.|
+|irr|Compute the Internal Rate of Return of a series of cash flows.|
+|fv|Compute the future value.|
+|npv|Compute the Net Present Value of a series of cash flows.|
+|pmt|Compute the payment against loan principal plus interest.|
+|ppmt|Compute the payment against loan principal.|
+|pv|Compute the present value.|
+
--- Sources/SwiftFinancial/SwiftFinancial.swift
+++ Sources/SwiftFinancial/SwiftFinancial.swift
@@ -1,6 +1,106 @@
-public struct SwiftFinancial {
-    public private(set) var text = "Hello, World!"
+//
+//  Amortization.swift
+//  RateSim
+//
+//  Created by Zachary Luety on 1/16/22.
+//
 
-    public init() {
+import Foundation
+
+// Compute the payment against loan principal plus interest.
+func pmt(rate: Double, numPeriods: Int, presentValue: Double, futureValue: Double = 0, whenDue: Int = 0) -> Double {
+    
+    let temp: Double = pow(1 + rate, Double(numPeriods))
+    let fact: Double = (1 + rate * Double(whenDue)) * (temp - 1) / rate
+    return -(futureValue + presentValue * temp) / fact
+}
+
+// Compute the present value.
+func pv(rate: Double, numPeriods: Int, payment: Double, futureValue: Double, whenDue: Int = 0) -> Double {
+    
+    let temp: Double = pow(1 + rate, Double(numPeriods))
+    let fact: Double = (1 + rate * Double(whenDue)) * (temp - 1) / rate
+    
+    return -(futureValue + payment * fact) / temp
+}
+
+
+// Compute the future value.
+func fv(rate: Double, numPeriods: Int, payment: Double, presentValue: Double, whenDue: Int = 0) -> Double {
+    
+    let temp: Double = pow(1 + rate, Double(numPeriods))
+    let fact: Double = (1 + rate * Double(whenDue)) * (temp - 1) / rate
+    
+    return -presentValue * temp - payment * fact
+}
+
+// Compute the interest portion of a payment.
+func ipmt(rate: Double, period: Int, numPeriods: Int, presentValue: Double, futureValue: Double = 0, whenDue: Int = 0) -> Double {
+    
+    let totalPayment: Double = pmt(rate: rate, numPeriods: numPeriods, presentValue: presentValue, futureValue: futureValue, whenDue: whenDue)
+    
+    let remainingBalance = fv(rate: rate, numPeriods: period - 1, payment: totalPayment, presentValue: presentValue, whenDue: whenDue)
+    
+    return remainingBalance * rate
+}
+
+// Compute the payment against loan principal.
+func ppmt(rate: Double, period: Int, numPeriods: Int, presentValue: Double, futureValue: Double = 0, whenDue: Int = 0) -> Double {
+    
+    let totalPayment: Double = pmt(rate: rate, numPeriods: numPeriods, presentValue: presentValue, futureValue: futureValue, whenDue: whenDue)
+    
+    return totalPayment - ipmt(rate: rate, period: period, numPeriods: numPeriods, presentValue: presentValue, futureValue: futureValue, whenDue: whenDue)
+}
+
+// Compute the Internal Rate of Return of a series of cash flows.
+func irr(cashFlows: [Double], estimate: Double? = nil) -> Double {
+    var iteration: Int = 1
+    let n: Int = cashFlows.count - 1
+    let exponent: Double = 1.0 / (1.0 + 0.5 * (Double(n) - 1.0)) - 1.0
+    
+    
+    // Pascual, Nerio & Sison, Ariel & Gerardo, Bobby & Medina, Ruji. (2018). Calculating Internal Rate of Return (IRR) in Practice using Improved Newton-Raphson Algorithm. Philippine Computing Journal. 13. 17-21.
+    var guess: Double = estimate ?? pow(cashFlows[1...].reduce(0, +) / -cashFlows[0], exponent)
+    var nextGuess: Double
+    
+    while abs(npv(rate: guess, cashFlows: cashFlows)) >= 0.0000000000001 && iteration <= 10 {
+        nextGuess = guess - npv(rate: guess, cashFlows: cashFlows) / dnpv(rate: guess, cashFlows: cashFlows)
+        guess = nextGuess
+        iteration += 1
     }
+    
+    return guess
+
+}
+
+// Compute the Net Present Value of a series of cash flows.
+func npv(rate: Double, cashFlows: [Double]) -> Double {
+    let periods = 0...cashFlows.count
+    let discountFactors = periods.map {1 / pow(1 + rate, Double($0))}
+    
+    var discountedCashFlows = [Double]()
+    for (cashFlow, discountFactor) in zip(cashFlows, discountFactors) {
+        discountedCashFlows.append(cashFlow * discountFactor)
+    }
+    
+    return discountedCashFlows.reduce(0, +)
+}
+
+private func dnpv(rate: Double, cashFlows: [Double]) -> Double {
+    let periods = 0...cashFlows.count
+    let discountFactors = periods.map { 1 / pow(1 + rate, Double($0 + 1))}
+    
+    let zipped = Array(zip(cashFlows, discountFactors))
+    
+    var values = [Double]()
+    var cashFlow: Double
+    var discountFactor: Double
+
+    for (index, item) in zipped.enumerated() {
+        cashFlow = item.0
+        discountFactor = item.1
+        values.append(Double(-index) * cashFlow * discountFactor)
+    }
+    
+    return values.reduce(0, +)
 }
